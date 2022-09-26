d3011760d281990227979f77172bce972707cf84
Anna Spitz <aspitzemail@gmail.com>
2022-09-25 20:24:42 -0500

    Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+      <footer>
+        <span>
+          Portions of Playset are unofficial Fan Content permitted under the
+          Wizards of the Coast Fan Content Policy. The literal and graphical
+          information presented on this site about Magic: The Gathering,
+          including card images, the mana symbols, and Oracle text, is copyright
+          Wizards of the Coast, LLC, a subsidiary of Hasbro, Inc. Playset is
+          not produced by, endorsed by, supported by, or affiliated with Wizards
+          of the Coast.
+        </span>
+      </footer>
+    );
+}
+
+export default Footer;
