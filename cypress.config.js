b97d064c5ea5eaf9bcd58ed3ff01f9906737fc2b
Anna Spitz <aspitzemail@gmail.com>
2022-10-13 10:56:11 -0500

    Add cypress record

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'mf5ec4',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
